85f83e3c19a607e324d0bc031b7ee1019bac1040
cylhah <951045989@qq.com>
2018-09-05 11:48:44 +0800

    first

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const Koa = require('koa')
+const { normal } = require('./tpl/normal')
 const app = new Koa()
 app.use(async (ctx, next) => {
-  ctx.body = '电影首页'
+  ctx.type = 'text;html,charset=utf-8'
+  ctx.body = normal
+  console.log('tag', '12323')
 })
 app.listen(4455)
